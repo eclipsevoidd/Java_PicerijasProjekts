fd4feb78ac06644550ea2f64545b4166b630f4b6
eclipsevoidd <gustavs.lacis@sk.lvt.lv>
2026-01-18 02:50:57 +0200

    Pilnveidota dinamiska ritjostlas pielāgošanās & komponentu ievade tajā

--- src/picerija/PicerijasDarbinieks.java
+++ src/picerija/PicerijasDarbinieks.java
@@ -1,10 +1,14 @@
 package picerija;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Random;
 
 import javax.swing.AbstractAction;
@@ -33,14 +37,66 @@ public class PicerijasDarbinieks {
     
     static JButton registretPoga;
     static JButton apskatitPoga;
+    static JButton vesturePoga;
     static JButton exitPoga;
+    
+    static JPanel saturaPanel;
+    
+    static JScrollPane ritinamaZona;
+    
+    public static void atjaunotPanelaIzmeru(JPanel panelis) {
+        int maxBottom = 0;
+        
+        for (Component comp : panelis.getComponents()) {
+            int elementaApaksa = comp.getY() + comp.getHeight();
+            
+            if (elementaApaksa > maxBottom) {
+                maxBottom = elementaApaksa;
+            }
+        }
+        panelis.setPreferredSize(new Dimension(panelis.getWidth(), maxBottom + 20));
+        
+        // Tell the scroll pane to update its bars
+        panelis.revalidate();
+        panelis.repaint();
+    }
+    
+    static void setElementi(String nosaukums) {
+    	// piekļust jpanel lodziņam, kurš saturēs elementus tajā
+        saturaPanel = (JPanel) ritinamaZona.getViewport().getView();
+        
+    	switch(nosaukums) {
+    	case "registret":
+    		for (int i = 0; i < 20; i++) {
+    			JLabel pasutijumsTitle = new JLabel("pizza!");
+                pasutijumsTitle.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 18));
+                pasutijumsTitle.setBounds(20, (10 + 35) * i, 300, 30);
+                saturaPanel.add(pasutijumsTitle);
+                
+                ImageIcon picaImg = new ImageIcon("atteli/pica.png");
+                JLabel pica = new JLabel(picaImg);
+                pica.setBounds(0, 42 * i, 32, 32);
+                saturaPanel.add(pica);
+                
+    		}
+    		
+    		break;
+    	case "apskatit":
+    		
+    		break;
+    	default:
+    		
+    		break;
+    	}
+    	atjaunotPanelaIzmeru(saturaPanel);
+    }
     
     public static JScrollPane izveidoRitinamoPaneli() {
         JPanel saturaPanelis = new JPanel();
         saturaPanelis.setLayout(null);
-        saturaPanelis.setBackground(new Color(240, 240, 240));
+        saturaPanelis.setBackground(new Color(230, 230, 230));
         
-        saturaPanelis.setPreferredSize(new java.awt.Dimension(600, 1000));
+        saturaPanelis.setPreferredSize(new Dimension(600, 0));
         
         JScrollPane scrollPane = new JScrollPane(saturaPanelis);
         scrollPane.setBounds(330, 190, 620, 450);
@@ -76,19 +132,47 @@ public class PicerijasDarbinieks {
 		teksts.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		registretPoga = new JButton("Jauns pasūtījums");
+		registretPoga.setName("registret");
 		registretPoga.setFocusPainted(false);
         registretPoga.setFont(new Font("Franklin Gothic Medium", Font.BOLD, 25));
         registretPoga.setBounds(70, 200, 250, 50);
         
+        registretPoga.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setElementi("registret");
+				registretPoga.setEnabled(false);
+			}
+        });
+        
         apskatitPoga = new JButton("Pasūtījumi");
+        apskatitPoga.setName("apskatit");
 		apskatitPoga.setFocusPainted(false);
         apskatitPoga.setFont(new Font("Franklin Gothic Medium", Font.BOLD, 25));
         apskatitPoga.setBounds(70, 255, 250, 50);
         
+        vesturePoga = new JButton("Pasūtījumu vēsture");
+        vesturePoga.setName("vesture");
+		vesturePoga.setFocusPainted(false);
+        vesturePoga.setFont(new Font("Franklin Gothic Medium", Font.BOLD, 25));
+        vesturePoga.setBounds(70, 310, 250, 50);
+        
         exitPoga = new JButton("Iziet");
+        exitPoga.setName("iziet");
 		exitPoga.setFocusPainted(false);
         exitPoga.setFont(new Font("Franklin Gothic Medium", Font.BOLD, 25));
-        exitPoga.setBounds(63, 590, 265, 50);
+        exitPoga.setBounds(63, 589, 265, 50);
+        exitPoga.setBackground(new Color(255, 170, 180));
+        
+        exitPoga.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent evt) {
+                exitPoga.setBackground(new Color(255, 125, 145));
+            }
+
+            public void mouseExited(MouseEvent evt) {
+                exitPoga.setBackground(new Color(255, 170, 180));
+            }
+        });
         
         exitPoga.addActionListener(new ActionListener() {
 			@Override
@@ -111,34 +195,19 @@ public class PicerijasDarbinieks {
         bgBlock2.setBounds(63, 190, 265, 400);
         bgBlock2.setEnabled(false);
         
-        JScrollPane ritinamaZona = izveidoRitinamoPaneli();
-        
-        // piekļust jpanel lodziņam, kurš saturēs elementus tajā
-        JPanel contentPanel = (JPanel) ritinamaZona.getViewport().getView();
-        
-        for (int i = 1; i <= 10; i++) {
-            JLabel orderLabel = new JLabel("Pasūtījums #" + i);
-            orderLabel.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 18));
-            orderLabel.setBounds(20, (i-1) * 80, 300, 30);
-            contentPanel.add(orderLabel);
-            
-            JLabel detailsLabel = new JLabel("Detaļas par pasūtījumu...");
-            detailsLabel.setBounds(20, (i-1) * 80 + 35, 300, 20);
-            contentPanel.add(detailsLabel);
-        }
+        ritinamaZona = izveidoRitinamoPaneli();
         
         bground.add(ritinamaZona);
         
         bground.add(registretPoga);
         bground.add(apskatitPoga);
+        bground.add(vesturePoga);
         bground.add(exitPoga);
         bground.add(bgBlock2);
         bground.add(teksts);
         bground.add(bgBlock);
         bground.add(bgBilde1);
         bground.add(bgBilde2);
-        
-        
         
         logs.add(bground);
         
@@ -167,7 +236,7 @@ public class PicerijasDarbinieks {
 		logs.setVisible(true);
 		timer.start();
 		
-		Action closeAction = new AbstractAction() {
+		Action aizvertArEsc = new AbstractAction() {
 			private static final long serialVersionUID = -244035018929600442L;
 
 			@Override
@@ -179,9 +248,7 @@ public class PicerijasDarbinieks {
 		logs.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
 		    .put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "aizvertLogu");
 
-		logs.getRootPane().getActionMap().put("aizvertLogu", closeAction);
-		
-		
+		logs.getRootPane().getActionMap().put("aizvertLogu", aizvertArEsc);
 		
 	}
 
